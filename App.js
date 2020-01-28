5bdc228d14b365a7160d114c6b51a7ec8099ff83
Sean Ross <sean.ross.985@gmail.com>
2020-01-28 10:28:51 -0800

    first commit, started with normal add button and got basic functionality working on the front end

---
+++ App.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList} from 'react-native';
+
+export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoal(enteredText)
+  }
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...courseGoals, { key: Math.random().toString(), value: enteredGoal}]);
+    setEnteredGoal('')
+  }
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+        placeholder="Course Goal" 
+        style={styles.input}
+        onChangeText={goalInputHandler}
+        value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <FlatList data= {courseGoals} renderItem={itemData => 
+      <View style={styles.listItem}>
+        <Text>{itemData.item.value}</Text>
+      </View> }
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row", 
+    justifyContent: "space-between", 
+    alignItems: "center" 
+  },
+  input: {
+    width: "80%", 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+
+});
